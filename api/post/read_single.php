f69652c25fbdd2f4176f954a76ccf0d5f5404c45
rokydas <droky136@gmail.com>
2021-10-22 15:23:00 +0600

    initial

---
+++ api/post/read_single.php
@@ -0,0 +1,33 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../model/Post.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate blog post object
+    $post = new Post($db);
+
+    // Get ID
+    $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+    // Get single post
+    $post->read_single();
+
+    // create array
+    $post_arr = array(
+        'id' => $post->id,
+        'title' => $post->title,
+        'body' => $post->body,
+        'author' => $post->author,
+        'category_id' => $post->category_id,
+        'category_name' => $post->category_name
+    );
+
+    // make json
+    print_r(json_encode($post_arr));
